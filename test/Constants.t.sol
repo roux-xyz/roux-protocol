1a0318226f433f5088b91e69c4e49357048eb99a
Maks Pazuniak <42282874+maks-p@users.noreply.github.com>
2024-04-04 08:47:37 -0400

    contracts: replace oz token base contracts with solady (#10)

--- test/Constants.t.sol
+++ test/Constants.t.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.24;
 
 uint64 constant TEST_TOKEN_MAX_SUPPLY = type(uint64).max;
 uint128 constant TEST_TOKEN_PRICE = 0.05 ether;
-
 uint32 constant TEST_TOKEN_MINT_DURATION = 365 days;
-
 string constant TEST_TOKEN_URI = "https://token.com/uri";
+string constant TEST_COLLECTION_NAME = "Test Collection";
+string constant TEST_COLLECTION_SYMBOL = "TST";
